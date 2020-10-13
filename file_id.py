39b2a074351a5a0c68dbe5b35be8de14ead455b3
Javohir <javohirjalilov777@gmail.com>
2020-10-13 14:06:30 +0500

    photo and sticker import file id

--- file_id.py
+++ file_id.py
@@ -1,24 +1,9 @@
 from telegram.ext import Updater, CommandHandler,MessageHandler,Filters
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ReplyKeyboardMarkup,ReplyMarkup,KeyboardButton
-
 import telegram
 import os
 
 TOKEN = os.environ['frilanser']
-
-#rn9 = '''Model: Redmi note 9 \nGPU: Mali-G71 MP20 \nOrqa kamera: 48+8+5+2 MP \nOld kamera: 13 MP \nROM: 64 GB \nRAM: 3GB \nBatareka: 5020 mAh \nTezkor quvvatlash: 18W'''
-
-#file_id = 'AgACAgIAAxkBAAIBwl-FYSs6GWAwD6YvT9H9H8_oH5YDAAJzrzEbgAcoSHs_eiEoSoG0QWkYlS4AAwEAAwIAA3kAA8A4BQABGwQ'
-#bot = telegram.Bot(TOKEN)
-#data = bot.getUpdates()
-#file_photo = open('redmi.jpg','rb')
-
-#print(data[-1])
-#bot.sendPhoto(
-#    chat_id=1046157991,
-#    photo=file_id,
-#    caption=rn9
-#)
 
 def get_photo(update, context):
     bot = context.bot
@@ -31,9 +16,21 @@ def get_photo(update, context):
         caption=f'https://t.me/JalilovJavohir\nFile_id: {file_id}'
     )
 
+def get_sticker(update, context):
+    bot = context.bot
+    chat_id = update.message.chat.id 
+    file_id = update.message.sticker.file_id
+    
+    bot.sendSticker(
+        chat_id=chat_id,
+        sticker=file_id
+    )
+    bot.sendMessage(chat_id,text=f'file_id: {file_id}')
+
 updater = Updater(token=TOKEN,use_context=True)
 
 updater.dispatcher.add_handler(MessageHandler(Filters.photo,get_photo))
+updater.dispatcher.add_handler(MessageHandler(Filters.sticker,get_sticker))
 
 
 updater.start_polling()
