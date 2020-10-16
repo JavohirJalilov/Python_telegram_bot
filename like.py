ea6c7c5c8e8ef7ff2d78114f4faf913eec205f96
Javohir <javohirjalilov777@gmail.com>
2020-10-17 00:29:37 +0500

    back ishlamadi hali

--- like.py
+++ like.py
@@ -5,28 +5,29 @@ import os
 
 TOKEN = os.environ['echo']
 
+def start(update,context):
 
-def start(update, context):
-
-    button = InlineKeyboardButton(
+    button1 = InlineKeyboardButton(
         text='Like and Dislike',
         callback_data='like_dislike'
     )
+    button2 = InlineKeyboardButton(
+        text='Mobile and desctop',
+        callback_data='Telegram_desctop'
+    )
     reply_markup = InlineKeyboardMarkup(
         inline_keyboard=[
-            [button]
+            [button1,button2]
         ]
     )
-
     bot = context.bot
     text = update.message.text
     chat_id = update.message.chat.id
     bot.sendMessage(
         chat_id=chat_id,
-        text='Like our bot!',
+        text='Like, dislike and Mobile, desctop',
         reply_markup=reply_markup
     )
-
 
 def help_bot(update, context):
     bot = context.bot
@@ -36,46 +37,113 @@ def help_bot(update, context):
 
 list1=[]
 list2=[]
-def inline1(update, context):
+def like(update, context):
+    query = update.callback_query
     callbackquery = update.callback_query.data
     if callbackquery=='like':
         list1.append('a')
-    elif callbackquery=='dislike':
+    if callbackquery=='dislike':
         list2.append('b')
     like = len(list1)
     dislike = len(list2)
+
     button1 = InlineKeyboardButton(
         text=f'👍🏻 {like}',
         callback_data='like'
     )
-    
     button2 = InlineKeyboardButton(
         text=f'👎🏻 {dislike}',
         callback_data='dislike'
     )
+    button3 = InlineKeyboardButton(
+        text = '🔙back',
+        callback_data='back'
+    )
 
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [button1,button2],
+            [button3]
+        ]
+    )
+    print(like,dislike)
+    query.edit_message_text('thank you very much!',reply_markup=reply_markup)
+    data = query.data
+    query.answer(
+        text='🤨😊😁😂'
+    )
+
+list3=[]
+list4=[]
+def mobile(update,context):
+    query = update.callback_query
+    callbackquery = update.callback_query.data
+    if callbackquery=='mobile':
+        list3.append('c')
+    if callbackquery=='desctop':
+        list4.append('d')
+    mobile = len(list3)
+    desctop = len(list4)
+
+    button1 = InlineKeyboardButton(
+        text=f'📱 {mobile}',
+        callback_data='mobile'
+    )
+    button2 = InlineKeyboardButton(
+        text=f'💻 {desctop}',
+        callback_data='desctop'
+    )
+    button3 = InlineKeyboardButton(
+        text = '🔙back',
+        callback_data='back'
+    )
+
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [button1,button2],
+            [button3]
+        ]
+    )
+    print(mobile,desctop)
+    query.edit_message_text('Telegramga qaysi qurilmadan kirasiz?',reply_markup=reply_markup)
+    data = query.data
+    query.answer(
+        text='🤨😊😁😂'
+    )
+
+def back(update,context):
+
+    query = update.callback_query
+    
+    button1 = InlineKeyboardButton(
+        text='Like and Dislike',
+        callback_data='like_dislike'
+    )
+    button2 = InlineKeyboardButton(
+        text='Mobile and desctop',
+        callback_data='Telegram_desctop'
+    )
     reply_markup = InlineKeyboardMarkup(
         inline_keyboard=[
             [button1,button2]
         ]
     )
 
-    query = update.callback_query
-    text = query.message.text
-    print(like,dislike)
-    query.edit_message_text(f'thank you very much!', reply_markup=reply_markup)
+    query.edit_message_text('Like, dislike and Mobile, desctop',reply_markup=reply_markup)
     data = query.data
     query.answer(
-        text='🤨😊😁😂',
-        show_alert=True
+        text='🤨😊😁😂'
     )
-
-
 updater = Updater(TOKEN)
-
 updater.dispatcher.add_handler(CommandHandler('help', help_bot))
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(inline1))
+updater.dispatcher.add_handler(CallbackQueryHandler(like,pattern='like_dislike'))
+updater.dispatcher.add_handler(CallbackQueryHandler(like,pattern='like'))
+updater.dispatcher.add_handler(CallbackQueryHandler(like,pattern='dislike'))
+updater.dispatcher.add_handler(CallbackQueryHandler(mobile,pattern='Telegram_desctop'))
+updater.dispatcher.add_handler(CallbackQueryHandler(mobile,pattern='mobile'))
+updater.dispatcher.add_handler(CallbackQueryHandler(mobile,pattern='desctop'))
+updater.dispatcher.add_handler(CallbackQueryHandler(back,pattern='🔙back'))
 
 updater.start_polling()
 updater.idle()
